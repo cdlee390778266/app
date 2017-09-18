40053b61c521178000062ca8047ff9128641aa40
Lee <390778266@qq.com>
2017-09-18 18:27:27 +0800

    细节调整

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -56,7 +56,10 @@ angular.module('starter.controllers', [])
     $scope.pieOption = {
       title : {
           text: '2017年中报',
-          x:'center'
+          textStyle: {
+            fontWeight: 'normal',
+            fontSize: 14
+          }
       },
       tooltip : {
           //trigger: 'item',
@@ -65,19 +68,32 @@ angular.module('starter.controllers', [])
       legend: {
           orient: 'vertical',
           right: 0,
-          top: 'middle',
+          top: '25%',
           data: data
       },
       series : [
           {
               name: '访问来源',
               type: 'pie',
-              radius : '55%',
-              center: ['50%', '60%'],
+              radius : '50%',
+              color:['#eb536c', '#7dc9fa','#c03bbb'],
+              label: {
+                normal: {
+                    show: false
+                },
+                emphasis: {
+                    show: false
+                }
+              },
+              labelLine: {
+                normal: {
+                  show: false
+                }
+              },
               data:[
-                  {value:1, name:'生态修复项目'},
-                  {value:2, name:'市政绿化项目'},
-                  {value:3, name:'地产景观'}
+                  {value:8, name:'生态修复项目'},
+                  {value:3, name:'市政绿化项目'},
+                  {value:1, name:'地产景观'}
               ],
               itemStyle: {
                   emphasis: {
@@ -101,22 +117,15 @@ angular.module('starter.controllers', [])
   Utils.getJSON('data/barh.json', function(data) {
     Utils.hidePop()
     $scope.barhOption = {
-      title: {
-          text: '总市值(单位：亿)',
-          subtext: '数据来自网络'
-      },
       tooltip: {
           trigger: 'axis',
           axisPointer: {
               type: 'shadow'
           }
       },
-      legend: {
-          
-      },
       grid: {
           left: '3%',
-          right: '4%',
+          right: '10%',
           bottom: '3%',
           containLabel: true
       },
@@ -148,7 +157,15 @@ angular.module('starter.controllers', [])
           {
               name: '2011年',
               type: 'bar',
-              data: data
+              data: data.reverse(),
+              itemStyle: {   
+                normal:{  
+                    color: function (params){
+                        var colorList = ['#ed9943', '#f24a64', '#16a1e4', '#7766cc', '#aa8abc', '#eb536c'].reverse();
+                        return colorList[params.dataIndex];
+                    }
+                }
+            }
           }
       ]
     }
@@ -164,9 +181,6 @@ angular.module('starter.controllers', [])
   Utils.getJSON('data/barv.json', function(data) {
     Utils.hidePop()
     $scope.lineOption = {
-      title: {
-          text: '折线图堆叠'
-      },
       tooltip: {
           trigger: 'axis'
       },
@@ -174,54 +188,31 @@ angular.module('starter.controllers', [])
           data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
       },
       grid: {
-          left: '3%',
-          right: '4%',
+          left: '0%',
+          right: '0%',
           bottom: '3%',
           containLabel: true
-      },
-      toolbox: {
-          feature: {
-              saveAsImage: {}
-          }
       },
       xAxis: {
           type: 'category',
           boundaryGap: false,
-          data: ['周一','周二','周三','周四','周五','周六','周日']
+          min: -1,
+          max: 6,
+
+          axisLabel :{  
+              interval:0   
+          },
+          data: ['2014\n年年报','2015\n年年报','2016\n年年报','2017\n年年报','2017\n一季报','2017\n年中报']
       },
       yAxis: {
           type: 'value'
       },
       series: [
         {
-          name:'邮件营销',
           type:'line',
           stack: '总量',
-          data:[120, 132, 101, 134, 90, 230, 210]
-        },
-        {
-          name:'联盟广告',
-          type:'line',
-          stack: '总量',
-          data:[220, 182, 191, 234, 290, 330, 310]
-        },
-        {
-          name:'视频广告',
-          type:'line',
-          stack: '总量',
-          data:[150, 232, 201, 154, 190, 330, 410]
-        },
-        {
-          name:'直接访问',
-          type:'line',
-          stack: '总量',
-          data:[320, 332, 301, 334, 390, 330, 320]
-        },
-        {
-          name:'搜索引擎',
-          type:'line',
-          stack: '总量',
-          data:[820, 932, 901, 934, 1290, 1330, 1320]
+          color: ['#6aa8e5'],
+          data:[1.4, 1.9, 2.8, 0.6, 1.4, 1.2]
         }
       ]
     }
@@ -403,23 +394,103 @@ angular.module('starter.controllers', [])
   Utils.showPop();
 
   Utils.getJSON('data/barv.json', function(data) {
-    Utils.hidePop()
-    $scope.gaugeOption = {
+    $scope.p = {
+      // title: {
+      //   text: '综合评分在银行行业25家',
+      //   subtext: '公司中名列：',
+      //   textStyle: {
+      //     color: '#666',
+      //     fontSize: 14,
+      //     fontWeight: 'normal'
+      //   }
+      // },
       tooltip : {
           formatter: "{a} <br/>{b} : {c}%"
-      },
-      toolbox: {
-          feature: {
-              restore: {},
-              saveAsImage: {}
-          }
       },
       series: [
           {
               name: '业务指标',
               type: 'gauge',
-              detail: {formatter:'{value}%'},
-              data: [{value: 50, name: '完成率'}]
+              startAngle: 180,
+              endAngle: 0,
+              min: 0,
+              max: 25,
+              splitNumber: 5,
+              pointer: {
+                length: '30%'
+              },
+              axisLine: {            // 坐标轴线
+                lineStyle: {       // 属性lineStyle控制线条样式
+                    color: [[0.2, '#c33430'],[0.8, '#d38310'],[1, '#399437']],
+                    width: 20
+                }
+            },
+              // detail: {
+              //   formatter:'第【{value}】名',
+              //   color: '#333',
+              //   fontSize: 18
+              // },
+              data: [{value: 5, name: ''}]
+          }
+      ]
+    }
+
+    Utils.hidePop()
+    $scope.gaugeOption = {
+      tooltip : {
+          formatter: "{a} <br/>{b} : {c}%"
+      },
+      series: [
+          {
+              name: '业务指标',
+              type: 'gauge',
+              startAngle: 180,
+              endAngle: 0,
+              min: 0,
+              max: 25,
+              splitNumber: 5,
+              pointer: {
+                length: '30%'
+              },
+              axisLine: {            // 坐标轴线
+                lineStyle: {       // 属性lineStyle控制线条样式
+                    color: [[0.2, '#c33430'],[0.8, '#d38310'],[1, '#399437']],
+                    width: 20
+                }
+            },
+            detail: {
+              formatter: [
+                '{a|第} {b|【{value}】} {a|名}',
+                '{c|环球排名持平     评分74.5} {d|良}'
+              ].join('\n'),
+              rich: {
+                  a: {
+                   color: '#666',
+                   fontSize: 14
+                  },
+                  b: {
+                    color: '#c33430',
+                    fontSize: 16,
+                    fontWeight: 'bold'
+                  },
+                  c: {
+                    height: 40,
+                    color: '#333',
+                    fontSize: 14
+                  },
+                  d: {
+                    width: 24,
+                    height: 24,
+                    lineHeight: 24,
+                    fontSize: 14,
+                    color: '#fff',
+                    backgroundColor: '#e5621c'
+                  } 
+              }
+
+            },
+
+            data: [{value: 5, name: ''}]
           }
       ]
     }
